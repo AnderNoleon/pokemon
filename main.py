cea54dd13024546ffb987da93fd62ae565959cd1
AnderNoleon <79151721+AnderNoleon@users.noreply.github.com>
2021-09-22 10:55:27 -0600

    Update main.py
    Base del menu

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 #comiendo
+
+
+nombre = input('Cual es su nombre')
+pokemon_inicial = input('Cual sera su pokemon inicial - Bulbasaur, Charmander o Squirtle.')
+
+
+while True:
+
+    print("Menu principal:") 
+    print('\nAcciones:')
+    print('1- Equipo Pokémon.')
+    print('2- Batallas contra Pokémon salvajes:')
+    print('3- Pokédex.:')
+    print('4-  Tienda.')
+    print('5- Salir.')
+
+    opcion = int(input('Seleccione una acción: '))
+    if opcion == 1:
+        pass
+    elif opcion ==2:
+        pass 
+    elif opcion ==3:
+        pass
+    elif opcion ==4:
+        pass
+    elif opcion ==5:
+        break
+
