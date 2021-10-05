231f86823ce9abc41a18d9a44f743aaa903c9f2f
JavierVasquez54 <61485489+JavierVasquez54@users.noreply.github.com>
2021-10-05 01:17:36 -0600

    Create batalla.py

---
+++ batalla.py
@@ -0,0 +1,25 @@
+import random
+import time 
+import json 
+import requests
+import os
+
+class Batalla :
+    def __init__(self):
+        self 
+
+    def ataque():
+        pass
+
+    def captura():
+        pass
+
+    def mochila():
+        pass
+
+    def huir():
+        pass
+
+    def turnos():
+        while True:
+            pass
